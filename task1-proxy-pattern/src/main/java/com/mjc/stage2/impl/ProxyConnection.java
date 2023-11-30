0936896cc2f3344085627e2e1d1eb303e0f44078
THETOUCH <super.ilgiznex@gmail.com>
2023-11-30 19:59:35 +0600

    123

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,15 +1,31 @@
 package com.mjc.stage2.impl;
 
 
-public class ProxyConnection {
+import com.mjc.stage2.Connection;
+
+public class ProxyConnection implements Connection {
     private RealConnection realConnection;
+    private ConnectionPool connectionPool;
+    @Override
+    public void close() {
+        connectionPool = ConnectionPool.getInstance();
+        this.realConnection = realConnection;
+        connectionPool.getConnection();
+    }
+
+    @Override
+    public boolean isClosed() {
+        return realConnection.isClosed();
+    }
 
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
+        connectionPool = ConnectionPool.getInstance();
     }
 
     public void reallyClose() {
         // Write your code here!
+        connectionPool.releaseConnection(realConnection);
     }
     // Implement methods here!
 }
